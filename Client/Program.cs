de4c8569468427ea846c4f1b5ff188bafb544f0b
DESKTOP-3AJM04N\admin <14dh110113@gmail.com>
2017-10-05 11:45:03 +0700

    firt commit

---
+++ Client/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Client
+{
+
+    class Program
+    {
+        private const int BUFFER_SIZE = 1024;
+        private const int PORT_NUMBER = 9999;
+        static ASCIIEncoding encoding = new ASCIIEncoding();
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient client = new TcpClient();
+
+                // 1. connect
+                client.Connect("127.0.0.1", PORT_NUMBER);
+                Stream stream = client.GetStream();
+
+                Console.WriteLine("Connected to Server.");
+
+                while (true)
+                {
+                    Console.Write("Enter your name: ");
+
+                    string str = Console.ReadLine();
+                    var reader = new StreamReader(stream);
+                    var writer = new StreamWriter(stream);
+                    writer.AutoFlush = true;
+
+                    // 2. send
+                    writer.WriteLine(str);
+
+                    // 3. receive
+                    str = reader.ReadLine();
+                    Console.WriteLine(str);
+                    if (str.ToUpper() == "BYE")
+                        break;
+                }
+                // 4. Close
+                stream.Close();
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
+
+
---
+++ Server/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace Server
+{
+    class Program
+    {
+        private const int BUFFER_SIZE = 1024;
+        private const int PORT_NUMBER = 9999;
+        static ASCIIEncoding encoding = new ASCIIEncoding();
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPAddress address = IPAddress.Parse("127.0.0.1");
+
+                TcpListener listener = new TcpListener(address, PORT_NUMBER);
+
+                // 1. listen
+                listener.Start();
+
+                Console.WriteLine("Server started on " + listener.LocalEndpoint);
+                Console.WriteLine("Waiting for a connection...");
+
+                Socket socket = listener.AcceptSocket();
+                Console.WriteLine("Connection received from " + socket.RemoteEndPoint);
+
+                var stream = new NetworkStream(socket);
+                var reader = new StreamReader(stream);
+                var writer = new StreamWriter(stream);
+
+                while (true)
+                {
+                    // 2. receive
+                    string str = reader.ReadLine();
+                    if (str.ToUpper() == "EXIT")
+                    {
+                        writer.WriteLine("bye");
+                        break;
+                    }
+                    // 3. send
+                    writer.WriteLine("Hello " + str);
+                }
+                // 4. close
+                stream.Close();
+                socket.Close();
+                listener.Stop();
+
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
